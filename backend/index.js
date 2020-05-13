6a2383f3206d450d796e31e1b57bd1bfb0dc71ad
Julian Kamphausen <juliankamphausen@me.com>
2020-05-13 10:14:50 +0200

    monthly calculations working 🆒

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,10 @@ import { CHECKS } from './lib/checks';
 // constants
 const FILES = [
     //'2015-108266.CSV',
-    '2016-108266.CSV',
-    '2016-3657277.CSV',
+    //'2016-108266.CSV',
+    //'2016-3657277.CSV',
     '2017-108266.CSV',
-    '2017-3657277.CSV',
+    //'2017-3657277.CSV',
     //'2018-108266.CSV',
     //'2019-108266.CSV',
 ];
@@ -25,15 +25,33 @@ const FILES = [
 // variables
 
 function test(journal) {
-    let { ins, outs, transactions, balance, accounts, years, timespan } = journal;
-    console.log({ ins, outs, transactions, balance, accounts, years, timespan });
+    let { calculated: { ins, outs, transactions, balance, accounts, years, timespan, monthly } } = journal;
+    console.log({ ins, outs, transactions, balance, accounts, years, timespan, monthly });
+}
+
+function testM(journal) {
+    let { calculated: { ins, outs, transactions, balance, accounts, years, timespan, monthly } } = journal;
+    console.log(monthly);
 }
 
 const journal = getJournalFromData(readMultipleDatasets(FILES));
-test(journal);
+//test(journal);
 
-const journal2 = journal.getTimeFrame(new Date(2017, 3, 1), new Date(2017, 7, 31));
-test(journal2);
+const journal2 = journal.getSubset(new Date(2017, 3, 1), new Date(2017, 7, 31));
+//test(journal2);
+
+const journal3 = journal.getSubset(new Date(2017, 0, 1), new Date(2017, 11, 31));
+//test(journal3);
+testM(journal3);
+
+
+let sumMonthBalances = 0;
+journal.calculated.monthly.forEach(m => {
+    console.log(`${m.key}: ${m.calculated.balance}`);
+    sumMonthBalances += m.calculated.balance;
+});
+
+console.log({ months: sumMonthBalances, journal: journal.calculated.balance })
 
 /*
 journal.postings[0]
