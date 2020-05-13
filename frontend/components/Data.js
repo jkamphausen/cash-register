de93c61519f651458e8c04c01174718f8e9a7678
Julian Kamphausen <juliankamphausen@me.com>
2020-05-13 15:27:42 +0200

    working! 👍🏻

--- frontend/components/Data.js
+++ frontend/components/Data.js
@@ -2,12 +2,22 @@ import { useContext } from 'react';
 import { JournalContext } from './JournalContext';
 
 export default function Data() {
-    const journalData = useContext(JournalContext);
-    console.log(journalData);
+    const { journal } = useContext(JournalContext);
+    console.log(journal);
+
     return (
         <div>
             <h2>Data:</h2>
-            {journalData.hey}
+
+            {/* {journal.calculated.ins}
+            {journal.calculated.outs} =
+            {journal.calculated.balance}
+            ({journal.calculated.ins + journal.calculated.outs}) */}
+            <ul>
+                {journal.postings.map(posting => <li key={posting._id}>
+                    {posting.amount}
+                </li>)}
+            </ul>
         </div>
     )
 }
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react';
 import { JournalProvider } from './JournalContext';
 
+// Copy from server → state needs to be declared in order to work even without data in place
+const emptyState = {
+    calculated: {
+        ins: 0,
+        outs: 0,
+        balance: 0,
+        transactions: {
+            in: 0,
+            out: 0,
+            total: 0,
+        },
+        accounts: [],
+        years: [],
+        timespan: {
+            first: null,
+            last: null,
+        },
+        monthly: [],
+    },
+    postings: [],
+}
+
+// Custom Hook!
+function useJournal() {
+    const [journal, setJournal] = useState({ ...emptyState });
+    useEffect(function () {
+        (async () => {
+            console.log('Mounting or Updating');
+            const res = await fetch('http://localhost:1848/journal');
+            const data = await res.json()
+            setJournal(data);
+        })()
+    },
+        []);
+    return journal;
+}
+
 export default function Page({ children }) {
+    const journal = useJournal();
     return (
-        <JournalProvider value={{ hey: 'ho', lets: 'go' }}>
+        <JournalProvider value={{ journal }}>
             <div className="page">
                 {children}
             </div>
